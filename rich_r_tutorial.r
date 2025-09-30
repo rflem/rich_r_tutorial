6364c86cb82863da8d0eacd32d156b8dceb5ff1c
rflem <richard.fleming@gmail.com>
2025-09-30 21:46:01 +0100

    Initial edits to bring in first notes from stat545

---
+++ rich_r_tutorial.r
@@ -0,0 +1,11 @@
+# Restart session on startup and clear out environment
+
+# ctrl + l to clear the console
+
+# Running update.packages(ask = FALSE, checkBuilt = TRUE) in the console will update all packages
+
+# Updating github through:
+# brew outdated
+# and then
+# brew upgrade
+
