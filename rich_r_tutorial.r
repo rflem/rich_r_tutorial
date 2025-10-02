404c7c6a1bc6c54b911d3643e42a69702ccd9856
rflem <richard.fleming@gmail.com>
2025-10-02 21:55:34 +0100

    new changes

--- rich_r_tutorial.r
+++ rich_r_tutorial.r
@@ -1,10 +1,10 @@
-# Restart session on startup and clear out environment
+## Restart session on startup and clear out environment
 
-# ctrl + l to clear the console
+## ctrl + l to clear the console
 
-# Running update.packages(ask = FALSE, checkBuilt = TRUE) in the console will update all packages
+## Running update.packages(ask = FALSE, checkBuilt = TRUE) in the console will update all packages
 
-# Updating github through:
+## Updating github through:
 # brew outdated
 # and then
 # brew upgrade
